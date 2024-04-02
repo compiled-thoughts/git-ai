3bb0f5785693ea4c96d8008efb1f29002ffd8b26
codermarcos <coder.marcos@gmail.com>
2024-04-01 23:35:13 -0300

    🎨 refactor to use the same name to variables On branch temporary Changes to be committed: 	modified:   src/ai/mod.rs

--- src/ai/mod.rs
+++ src/ai/mod.rs
@@ -14,15 +14,22 @@ pub const AVAILABLE_AI: &[&str; 1] = &["OpenAI"];
 
 pub async fn generate_message(
     configuration: super::Configuration,
-    ticket: Vec<Ticket>,
+    tickets: Vec<Ticket>,
     diff: String,
 ) -> Result<String, reqwest::Error> {
     match &configuration.ai {
         AIConfiguration::OpenAI(open_ai_configuration) => {
             let commit_payload =
-                open_ai::get_prompt_for_commit(diff, ticket, configuration.instructions.commit);
+                open_ai::get_prompt_for_commit(
+                    diff, 
+                    tickets, 
+                    configuration.instructions.commit
+                );
 
-            let payload = open_ai::generate_message(commit_payload, open_ai_configuration).await?;
+            let payload = open_ai::generate_message(
+                commit_payload, 
+                open_ai_configuration
+            ).await?;
 
             let message = &payload["choices"][0]["message"]["content"];
 
